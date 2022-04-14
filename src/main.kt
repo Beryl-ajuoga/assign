b69cbea4e672487f82637e52181f02cd69caae19
beryl-ajuoga <berrylajuoga@gmail.com>
2022-04-14 09:40:37 +0300

    first commit

---
+++ src/main.kt
@@ -0,0 +1,21 @@
+fun main(){
+getevenName("girly")
+}
+fun getevenName(name:String):String{
+    println(name[2])
+    return name
+
+
+}
+class currentAccount(var accountNumber: Int ,accountName: String,balance:Int){
+    fun balance(deposit:Int):Int{
+       deposit+=balance(
+
+       )
+        return deposit
+    }
+}
+
+
+
+
